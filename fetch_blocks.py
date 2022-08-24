827b201748fff566e36db634dd55117a73bce30d
Mint <>
2022-08-24 11:38:44 +0300

    ua

--- fetch_blocks.py
+++ fetch_blocks.py
@@ -4,7 +4,7 @@ import sqlite3
 from bs4 import BeautifulSoup
 
 headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0"
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0"
 }
 
 
